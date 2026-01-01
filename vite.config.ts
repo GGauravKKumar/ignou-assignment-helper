29d6444cffd4a5dc6b0028df536d661b880302ba
Gaurav Jha <gaurav@SonOfAnton.SonOfAnton>
2026-01-01 22:09:40 +0530

    Up

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,8 @@ import { componentTagger } from "lovable-tagger";
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
-    port: 8080,
+    port: 8081,
+    allowedHosts: ["b55040e2f22f.ngrok-free.app", "localhost"],
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
